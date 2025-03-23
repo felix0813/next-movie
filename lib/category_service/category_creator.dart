85170dbf1a8973e28a3783f152773610bcc40cf6
felix <1306332027@qq.com>
2025-03-23 17:19:00 +0800

    在创建目录时检查重复

--- lib/category_service/category_creator.dart
+++ lib/category_service/category_creator.dart
@@ -2,6 +2,8 @@ import 'package:next_movie/model/category.dart';
 
 import 'package:next_movie/provider/objectbox_provider.dart';
 
+import '../objectbox/objectbox.g.dart';
+
 class CategoryCreator {
   late final ObjectBoxProvider? objectBoxProvider;
   final _category = Category();
@@ -11,15 +13,18 @@ class CategoryCreator {
   }
 
   bool create() {
-    //todo 检测重名
+    Box box=objectBoxProvider!.getBox<Category>();
     if (_category.name.isEmpty) {
       throw Exception("Category name cannot be empty");
     }
     if (objectBoxProvider == null) {
       throw Exception("ObjectBoxProvider is not initialized");
     }
+    if(box.query(Category_.name.equals(_category.name)).build().find().isNotEmpty){
+      throw Exception("Category name already exists");
+    }
     _category.created = DateTime.now().toLocal().toString().split(".")[0];
-    return objectBoxProvider!.getBox<Category>().put(_category) != 0;
+    return box.put(_category) != 0;
   }
 
   CategoryCreator({required this.objectBoxProvider});
